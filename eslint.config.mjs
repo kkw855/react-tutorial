7a9b4e380d1063ad7ff06c1ac09c0224a777a437
kkw855 <kkw855@gmail.com>
2024-06-04 22:26:26 +0900

    ✨ feat: ESLint flat config, Prettier 적용

---
+++ eslint.config.mjs
@@ -0,0 +1,54 @@
+/* eslint-disable */
+// noinspection JSCheckFunctionSignatures
+
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import reactRecommended from 'eslint-plugin-react/configs/recommended.js'
+import reactHooks from 'eslint-plugin-react-hooks'
+import jsxRuntime from 'eslint-plugin-react/configs/jsx-runtime.js'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import globals from 'globals'
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  reactRecommended,
+  jsxRuntime,
+  {
+    plugins: {
+      'react-hooks': reactHooks,
+    },
+    rules: reactHooks.configs.recommended.rules,
+  },
+  {
+    // only applied on tsx & jsx source files
+    plugins: {
+      'react-refresh': reactRefresh, // 빠른 새로 고침(HMR)을 통해 구성 요소를 안전하게 업데이트할 수 있게 도와줌
+    },
+    rules: {
+      'react-refresh/only-export-components': 'warn',
+    },
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: import.meta.dirname,
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser,
+      },
+    },
+    rules: {
+      // 타입스크립트 타입을 정의할 때 type or interface 키워드 중 하나만 사용하게 설정
+      '@typescript-eslint/consistent-type-definitions': 'off',
+    },
+  },
+  eslintConfigPrettier,
+)
