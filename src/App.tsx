19b78a117b3de6e80410cb70f5b2dfd0a5e3a5e5
kkw855 <kkw855@gmail.com>
2024-06-04 23:46:00 +0900

    ✨ feat: products 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 import Home from '@pages/Home.tsx'
+import Products from '@pages/Products.tsx'
 
-const router = createBrowserRouter([{ path: '/', element: <Home /> }])
+const router = createBrowserRouter([
+  { path: '/', element: <Home /> },
+  { path: '/products', element: <Products /> },
+])
 
 const App = () => {
   return <RouterProvider router={router} />
---
+++ src/pages/Products.tsx
@@ -0,0 +1,5 @@
+const ProductsPage = () => {
+  return <h1 className='text-3xl'>The Products Page</h1>
+}
+
+export default ProductsPage
